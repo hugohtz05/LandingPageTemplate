5082b4e5d457a5e656897c0412a07130865113ff
hugohtz05 <hugoheintz03@gmail.com>
2024-10-28 23:54:59 +0100

    firstpart finish :)

--- src/components/FisrtPartMainPage.vue
+++ src/components/FisrtPartMainPage.vue
@@ -3,88 +3,90 @@
 <template>
   <main>
     <nav>
-      <div class="xxs:flex xxs:justify-center">
-        <div
-          class="xxs:mt-[56px] xxs:w-[343px] xxs:h-[307px] xxs:mb-[55px] md:w-[689px] md:h-[482px] mx-auto xxl:w-[1110px] bg-slate-400 xxl:h-[600px]"
-        >
+      <div class="flex justify-center">
+        <div class="xxs:flex xxs:justify-center">
+          <div
+            class="xxs:mt-[56px] xxs:w-[343px] xxs:h-[307px] xxs:mb-[55px] md:w-[689px] md:h-[482px] mx-auto xxl:w-[824px] xxl:h-[600px]"
+          >
+            <img
+              class="xss:w-[151.47px] xxs:h-[19px] xxs:ml-[160px] md:w-[231px] md:h-[29px] md:ml-[221px] md:mt-[42px] md:z-10 md:relative xxl:w-[287px] xxl:h-[32px] xxl:ml-[296px] xxl:mt-[46px]"
+              src="/src/assets/pattern-curved-line-1.svg"
+              alt=""
+            />
+            <p
+              class="font-Epilogue text-blue xxs:text-[38px] xxs:leading-[44px] xxs:tracking-[-0.40px] xxs:mb-[32px] xxs:mt-[8pxpx] md:mt-[18px] md:text-[56px] md:leading-[64px] md:tracking-[-0.78px] md:mb-[40px] md:z-10 md:relative xxl:mt-[21px] xxl:text-[72px] xxl:leading-[78px] xxl:tracking-[-1px]"
+            >
+              A <span class="font-bold">super solution <br /> </span>for your
+              <span class="font-bold"> business.</span>
+            </p>
+            <p
+              class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[28px] xss:tracking-[0.09px] xxs:mb-[32px] md:w-[350px] md:h-[84px] md:mb-[40px] xxl:text-[18px] xxl:leading-[32px] xxl:tracking-[0.1px] xxl-mb-[52px]"
+            >
+              Our marketing and sales automations help you scale your outreach
+              to get more leads for your company.
+            </p>
+            <button
+              class="border-blue border-solid rounded-[6px] border-[2px] text-creamwhite font-bold bg-blue xxs:py-[12px] xxs:px-[32px] md:py-[15px]"
+            >
+              Request Beta Access
+            </button>
+          </div>
           <img
-            class="xss:w-[151.47px] xxs:h-[19px] xxs:ml-[160px] md:w-[231px] md:h-[29px] md:ml-[221px] md:mt-[42px] md:z-10 md:relative xxl:w-[287px] xxl:h-[32px] xxl:ml-[296px] xxl:mt-[46px]"
-            src="/src/assets/pattern-curved-line-1.svg"
+            class="absolute xxs:hidden md:inline md:w-[281px] md:h-[482px] md:ml-[463px] md:z-0 md:mt-[56px] md:mr-[3px] xxl:h-[600px] xxl:w-[350px]"
+            src="/src/assets/image-hero-portrait.png"
             alt=""
           />
-          <p
-            class="font-Epilogue text-blue xxs:text-[38px] xxs:leading-[44px] xxs:tracking-[-0.40px] xxs:mb-[32px] xxs:mt-[8pxpx] md:mt-[18px] md:text-[56px] md:leading-[64px] md:tracking-[-0.78px] md:mb-[40px] md:z-10 md:relative xxl:mt-[21px] xxl:text-[72px] xxl:leading-[78px] xxl:tracking-[-1px]"
-          >
-            A <span class="font-bold">super solution <br /> </span>for your
-            <span class="font-bold"> business.</span>
-          </p>
-          <p
-            class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[28px] xss:tracking-[0.09px] xxs:mb-[32px] md:w-[350px] md:h-[84px] md:mb-[40px] xxl:text-[18px] xxl:leading-[32px] xxl:tracking-[0.1px] xxl-mb-[52px]"
-          >
-            Our marketing and sales automations help you scale your outreach to
-            get more leads for your company.
-          </p>
-          <button
-            class="border-blue border-solid rounded-[6px] border-[2px] text-creamwhite font-bold bg-blue xxs:py-[12px] xxs:px-[32px] md:py-[15px]"
-          >
-            Request Beta Access
-          </button>
         </div>
-        <img
-          class="absolute xxs:hidden md:inline md:w-[281px] md:h-[482px] md:ml-[411px] md:z-0 md:mt-[56px] md:mr-[3px] xxl:h-[600px] xxl:w-[350px] xxl:mr-[220px]"
-          src="/src/assets/image-hero-portrait.png"
-          alt=""
-        />
-      </div>
 
-      <div class="xxs:flex xxs:justify-center">
-        <img
-          class="xxs:w-[343px] xxs:h-[240px] xxs:mb-[56px] md:hidden"
-          src="/src/assets/image-hero-landscape.png
+        <div class="xxs:flex xxs:justify-center">
+          <img
+            class="xxs:w-[343px] xxs:h-[240px] xxs:mb-[56px] md:hidden"
+            src="/src/assets/image-hero-landscape.png
         "
-          alt=""
-        />
-      </div>
+            alt=""
+          />
+        </div>
 
-      <div class="xxs:flex xxs:justify-center">
-        <div
-          class="xxs:w-[344px] xxs:h-[330px] xxs:flex xxs:flex-col xxs:items-center md:w-[689px] md:h-[83px] md:flex-row md:items-center md:justify-between"
-        >
-          <div class="xxs:flex xxs:flex-col xxs:items-center">
-            <h2
-              class="font-Epilogue font-bold text-blue xxs:text-[48px] xxs:leading-[56px]"
-            >
-              2K+
-            </h2>
-            <p
-              class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[26px] xxs:tracking-[2.5px] xxs:mb-[32px] md:mb-0"
-            >
-              COMPANIES
-            </p>
-          </div>
-          <div class="xxs:flex xxs:flex-col xxs:items-center">
-            <h2
-              class="font-Epilogue font-bold text-blue xxs:text-[48px] xxs:leading-[56px]"
-            >
-              8
-            </h2>
-            <p
-              class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[26px] xxs:tracking-[2.5px] xxs:mb-[32px] md:mb-0"
-            >
-              LANGUAGES
-            </p>
-          </div>
-          <div class="xxs:flex xxs:flex-col xxs:items-center">
-            <h2
-              class="font-Epilogue font-bold text-blue xxs:text-[48px] xxs:leading-[56px]"
-            >
-              1.2M
-            </h2>
-            <p
-              class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[26px] xxs:tracking-[2.5px] xxs:mb-[56px] md:mb-0"
-            >
-              LEADS
-            </p>
+        <div class="xxs:flex xxs:justify-center">
+          <div
+            class="xxs:w-[344px] xxs:h-[330px] xxs:flex xxs:flex-col xxs:items-center md:w-[689px] md:h-[83px] md:flex-row md:items-center md:justify-between xxl:w-[190px] xxl:h-[375px] xxl:flex-col xxl:mt-[173px] xxl:ml-[96px] xxl:items-start"
+          >
+            <div class="xxs:flex xxs:flex-col xxs:items-center xxl:items-start">
+              <h2
+                class="font-Epilogue font-bold text-blue xxs:text-[48px] xxs:leading-[56px]"
+              >
+                2K+
+              </h2>
+              <p
+                class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[26px] xxs:tracking-[2.5px] xxs:mb-[32px] md:mb-0"
+              >
+                COMPANIES
+              </p>
+            </div>
+            <div class="xxs:flex xxs:flex-col xxs:items-center xxl:items-start">
+              <h2
+                class="font-Epilogue font-bold text-blue xxs:text-[48px] xxs:leading-[56px]"
+              >
+                8
+              </h2>
+              <p
+                class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[26px] xxs:tracking-[2.5px] xxs:mb-[32px] md:mb-0"
+              >
+                LANGUAGES
+              </p>
+            </div>
+            <div class="xxs:flex xxs:flex-col xxs:items-center xxl:items-start">
+              <h2
+                class="font-Epilogue font-bold text-blue xxs:text-[48px] xxs:leading-[56px]"
+              >
+                1.2M
+              </h2>
+              <p
+                class="font-Epilogue text-grey xxs:text-[16px] xxs:leading-[26px] xxs:tracking-[2.5px] xxs:mb-[56px] md:mb-0"
+              >
+                LEADS
+              </p>
+            </div>
           </div>
         </div>
       </div>
