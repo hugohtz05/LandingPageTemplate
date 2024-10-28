52cc617d5752bd3dc61ebbf9765947918e349592
hugohtz05 <hugoheintz03@gmail.com>
2024-10-28 20:59:00 +0100

    second part main done

--- src/components/SecondPartMainPage.vue
+++ src/components/SecondPartMainPage.vue
@@ -3,12 +3,12 @@
     <nav>
       <div class="xxs:flex xxs:justify-center">
         <img
-          class="absolute xxs:mt-[56px] xxs:w-[auto] xxs:h-[402px] xxs:z-10"
+          class="absolute xxs:mt-[56px] xxs:w-[auto] xxs:h-[402px] xxs:z-10 md:mt-[92px]"
           src="/src/assets/image-jeremy-large.png"
           alt=""
         />
         <img
-          class="absolute xxs:h-[402px] xxs:m-[120px]"
+          class="absolute xxs:h-[402px] xxs:mt-[120px] md:mt-[156px]"
           src="/src/assets/pattern-blur.svg"
           alt=""
         />
@@ -16,7 +16,7 @@
 
       <div class="xxs:flex xxs:justify-center">
         <div
-          class="bg-blue rounded-[15px] xxs:mt-[237px] xxs:flex xxs:flex-col xxs:items-center xxs:h-[748px] xxs:w-[375px]"
+          class="bg-blue rounded-[15px] xxs:mt-[237px] xxs:flex xxs:flex-col xxs:items-center xxs:h-[748px] xxs:w-[375px] md:mt-[273px] md:h-[684px] md:w-[689px]"
         >
           <img
             class="xxs:h-[40px] xxs:w-[50px] xxs:mt-[261px] xxs:mb-[37px]"
@@ -29,7 +29,7 @@
             It just <span class="font-bold">Works.</span>
           </p>
           <p
-            class="font-Epilogue text-cream xxs:w-[343px] xxs:h-[160px] xxs:text-[18px] xxs:leading-[32px] xxs:tracking-[0.09px] xxs:mb-[40px]"
+            class="font-Epilogue text-cream xxs:w-[343px] xxs:h-[160px] xxs:text-[18px] xxs:leading-[32px] xxs:tracking-[0.09px] xxs:mb-[40px] xxs:text-center md:w-[573px] md:h-[96px] md:text-center"
           >
             “I really like how it is an all-in-one solution that handle many of
             the tasks that you would normally need separate tools to do the same
